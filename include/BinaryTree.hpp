3d1e977c6fb328a39bb956e6471eb948cdcb3ac9
krstau <krstau@stud.ntnu.no>
2022-05-20 05:06:29 +0200

    Removed various functions from BinaryTree.hpp.

--- include/BinaryTree.hpp
+++ include/BinaryTree.hpp
@@ -7,57 +7,21 @@
 // Inspiration for BinaryTree code: https://github.com/devos50/BinaryTree
 
 /**
- *
+ * BinaryTree class.
  * @tparam T Data input.
  */
 template<typename T>
 class BinaryTree {
 public:
     explicit BinaryTree(const T& data);
-    ~BinaryTree();
-    int getSize() const;
-    Node<T> *getRoot() const;
-    Node<T> *getParent();
-    Node<T> *getRightChild();
-    Node<T> *getLeftChild();
     void traverseDepthFirst(std::function<void(Node<T>*)>&) const;
-    void destroyRecursive(Node<T> *node);
     void insertNode(Node<T> *node, T data);
-    bool isEmpty();
-    bool isRoot();
-    bool hasLeftChild();
-    bool hasRightChild();
-    bool hasParent();
 
 private:
     Node<T> *root_ = nullptr;
     int size_ = 0;
     void traverseDepthFirst(Node<T> *root, std::function<void(Node<T>*)>&) const;
 };
-
-/**
- * BinaryTree destructor.
- * @tparam T
- */
-template<typename T>
-BinaryTree<T>::~BinaryTree(){
-    // destroyRecursive(root_);
-}
-
-/**
- *
- * @tparam T
- */
-template<typename T>
-void BinaryTree<T>::destroyRecursive(Node<T>* node)
-{
-    if (node)
-    {
-        destroyRecursive(node->getLeftPtr());
-        destroyRecursive(node->getRightPtr());
-        delete node;
-    }
-}
 
 /**
  * BinaryTree default constructor.
@@ -87,45 +51,10 @@ void BinaryTree<T>::insertNode(Node<T> *node, T data) {
     }
 }
 
-/**
- * Gets the amount of nodes in the BinaryTree.
- * @tparam T
- * @return size_ amount of nodes.
- */
-template<typename T>
-int BinaryTree<T>::getSize() const {
-    return size_;
-}
-
-/**
- * Checks if BinaryTree does not have any nodes.
- * @tparam T
- * @return true if BinaryTree has no nodes.
- */
-template<typename T>
-bool BinaryTree<T>::isEmpty() {
-    return (size_ == 0);
-}
-
-/**
- * Gets the root node of the BinaryTree.
- * @tparam T
- * @return true if BinaryTree has no nodes.
- */
-template<typename T>
-Node<T> *BinaryTree<T>::getRoot() const {
-    if(isEmpty()){
-        std::cout << "BinaryTree is empty!" << std::endl;
-        return nullptr;
-    }
-    else {
-        return root_;
-    }
-}
 
 /**
 * Recursive depth first traversal function.
-* @param rootnode.
+* @param root node.
 * @param nodeFunction.
 */
 template<typename T>
